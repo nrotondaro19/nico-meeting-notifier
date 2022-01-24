0373f59ffd42032933b5418e96565668719d66b3
hervster <hervenyemeck@gmail.com>
2022-01-24 17:02:04 -0500

    Local app completed, started server.io implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import nicoOne from './Nico1.jpg'
+import nicoTwo from './Nico2.jpg'
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const imagesPath = {
+    yes: nicoTwo,
+    no: nicoOne
+  }
+
+const backColor = {
+  no: '#0cf31f',
+  yes: '#fa2509'
+}
+  
+  class App extends React.Component {
+    state = {
+      open: true,
+      inMeeting: true
+    }
+    toggleImage = () => {
+      this.setState(state => ({ open: !state.open }))
+    }
+  
+    getImageName = () => this.state.open ? 'no' : 'yes'
+  
+    render() {
+      const imageName = this.getImageName();
+      return (  
+      <div className="App">
+      <header style={{backgroundColor: backColor[imageName]}} className="App-header">
+        <img style={{maxWidth: '400px'}} src={imagesPath[imageName]} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Are you in a <code>MEETING</code> ?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={this.toggleImage}>Indeed</button>
+        <div> Or nah </div>
+        <button onClick={this.toggleImage}>No</button>
       </header>
     </div>
   );
-}
+    }
+  }
+  
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<App />, rootElement);
 
-export default App;
+  export default App;
+
